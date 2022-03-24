5a6f34df6066e1b31056e4b9347f607fdb37b21d
Hee Jae <dark199174@gmail.com>
2022-03-24 14:43:20 +0900

    Add proxy to avoid CORS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/cj",
+        destination: `${process.env.NEXT_PUBLIC_API_ENDPOINT}?website-id=${process.env.NEXT_PUBLIC_WEBSITE_ID}&advertiser-ids=${process.env.NEXT_PUBLIC_ADVERTISER_IDS}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
