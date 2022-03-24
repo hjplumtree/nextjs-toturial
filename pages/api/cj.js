9a6f5a8ce4329da70975ff5928fac67a65ee1a0c
Hee Jae <dark199174@gmail.com>
2022-03-24 14:43:41 +0900

    Add xml to JSON

--- pages/api/cj.js
+++ pages/api/cj.js
@@ -1,26 +1,23 @@
 export default async function fetchCj() {
-  try {
-    const API_ENDPOINT = "https://link-search.api.cj.com/v2/link-search";
-    const TOKEN = "5nw1mv627qq58s035bc85k574a";
-    const WEBSITE_ID = 7928723;
-    const ADVERTISER_IDS = 21730313;
+  var parseString = require("xml2js").parseString;
 
-    const response = await fetch(`${API_ENDPOINT}?website-id=${WEBSITE_ID}`, {
+  try {
+    const response = await fetch("/cj", {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${TOKEN}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+        Accept: "application/xml",
       },
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
     });
 
     if (!response.ok) {
       throw new Error("HTTP ERROR Status:" + response.status);
     }
 
-    const data = await response.json();
-
-    console.log(data);
+    const xml = await response.text();
+    parseString(xml, (err, result) => {
+      console.log(result["cj-api"].links[0].link);
+    });
   } catch (e) {
     throw new Error("fetch ERROR message: " + e.message);
   }
