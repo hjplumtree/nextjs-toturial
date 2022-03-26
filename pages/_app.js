1def1fff860077138f49a4fc04a7a86a2575e36a
Hee Jae <dark199174@gmail.com>
2022-03-26 20:49:13 +0900

    Add global Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import "../styles/reset.css";
+import Layout from "../components/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <Layout>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </Layout>
   );
 }
 
